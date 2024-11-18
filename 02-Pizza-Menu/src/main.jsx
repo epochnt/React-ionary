40e654e39d321e89637470e4d0f92698992f556d
epochnt <ntt45419@gmail.com>
2024-11-18 12:28:41 +0530

    Added css,images,data and Updated App component for Pizza menu project

--- 02-Pizza-Menu/src/main.jsx
+++ 02-Pizza-Menu/src/main.jsx
@@ -1,10 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
 
-createRoot(document.getElementById('root')).render(
+import App from "./App";
+
+// React v18
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <App />
-  </StrictMode>,
-)
+  </StrictMode>
+);
+
+/* Before React 18
+   import ReactDOM from 'react-dom';
+   ReactDOMReact.render(<App />, document.getElementById('root'));
+*/
