c70b5494dd189090dcda181120d940e4e016f32b
epochnt <ntt45419@gmail.com>
2024-11-18 23:04:30 +0530

    Completed header menu and footer section

--- 02-Pizza-Menu/src/App.jsx
+++ 02-Pizza-Menu/src/App.jsx
@@ -1,23 +1,63 @@
+/* eslint-disable react/prop-types */
 import "./index.css";
-// import pizzaData from "./assets/data";
+import pizzaData from "./assets/data";
+
 function App() {
   return (
-    <div>
-      <h1>React</h1>
-      <Pizza />
-      <Pizza />
-      <Pizza />
+    <div className="container">
+      <Header />
+      <Menu pizzas={pizzaData} />
+      <Footer />
     </div>
   );
 }
 
-function Pizza() {
+function Header() {
   return (
-    <div>
-      <img src="/src/assets/pizzas/spinaci.jpg" alt="Pizza Spinaci" />
-      <h2>Pizza Spinaci</h2>
-      <p>Tomato, mozarella, spinach, and ricotta cheese</p>
-    </div>
+    <header className="header">
+      <h1>Fast React Pizza Co.</h1>
+    </header>
+  );
+}
+
+function Menu({ pizzas }) {
+  const pizzaList = pizzas.map((pizza, index) => (
+    <Pizza key={index} {...pizza} />
+  ));
+  return (
+    <menu className="menu">
+      <h2>Our Menu</h2>
+      <ul className="pizzas">{pizzaList}</ul>
+    </menu>
+  );
+}
+
+function Pizza({ name, ingredients, img, price }) {
+  return (
+    <li className="pizza">
+      <img src={img} alt={name} />
+      <div>
+        <h3>{name}</h3>
+        <p>{ingredients}</p>
+        <span>${price}</span>
+      </div>
+    </li>
+  );
+}
+
+function Footer() {
+  const hour = new Date().getHours();
+  const openHour = 12;
+  const closeHour = 22;
+  const isOpen = hour >= openHour && hour <= closeHour;
+  console.log(isOpen);
+  // if (hour > openHour && hour < closeHour) alert("we are open");
+
+  return (
+    <footer className="footer">
+      {new Date().toLocaleTimeString()}. We are currently open. Copyright &copy;
+      2023 Fast React Pizza Co.
+    </footer>
   );
 }
 
