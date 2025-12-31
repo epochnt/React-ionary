8b369021cfe427108c7db7e78165a39a7485e676
Nitin Thakur <ntt45419@gmail.com>
2026-01-01 01:09:49 +0530

    Nice key and position state example:HAPPY NEW YEAR

--- 03-Common-Modules-Smp/src/App.jsx
+++ 03-Common-Modules-Smp/src/App.jsx
@@ -1,12 +1,14 @@
-import Steps from "./SCA/Steps/Steps";
+// import Steps from "./SCA/Steps/Steps";
 // import DateCounter from "./Challenges/DateCounter";
 // import Accordian from "./Challenges/Accordian";
 // import Steps from "./Challenges/Steps_v2";
+import ContactList from "./Examples/ContactList/ContactList"
 
 function App() {
-  return <Steps />;
+  // return <Steps />;
   // return <DateCounter />;
   // return <Accordian />;
+  return <ContactList />
 }
 
 export default App;
