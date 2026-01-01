ac359746a4be55e9ed029ddbf131db103ed4f500
Nitin Thakur <ntt45419@gmail.com>
2026-01-01 16:49:36 +0530

    Added cleanups and cleaned up

--- usepopcorn/src/App.jsx
+++ usepopcorn/src/App.jsx
@@ -5,53 +5,6 @@ import "./index.css";
 const OMDB_BASE_URL = import.meta.env.VITE_OMDB_URL;
 const OMDB_API_KEY = import.meta.env.VITE_OMDB_API_KEY;
 
-const tempMovieData = [
-  {
-    imdbID: "tt1375666",
-    Title: "Inception",
-    Year: "2010",
-    Poster:
-      "https://m.media-amazon.com/images/M/MV5BMjAxMzY3NjcxNF5BMl5BanBnXkFtZTcwNTI5OTM0Mw@@._V1_SX300.jpg",
-  },
-  {
-    imdbID: "tt0133093",
-    Title: "The Matrix",
-    Year: "1999",
-    Poster:
-      "https://m.media-amazon.com/images/M/MV5BNzQzOTk3OTAtNDQ0Zi00ZTVkLWI0MTEtMDllZjNkYzNjNTc4L2ltYWdlXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_SX300.jpg",
-  },
-  {
-    imdbID: "tt6751668",
-    Title: "Parasite",
-    Year: "2019",
-    Poster:
-      "https://m.media-amazon.com/images/M/MV5BYWZjMjk3ZTItODQ2ZC00NTY5LWE0ZDYtZTI3MjcwN2Q5NTVkXkEyXkFqcGdeQXVyODk4OTc3MTY@._V1_SX300.jpg",
-  },
-];
-
-const tempWatchedData = [
-  {
-    imdbID: "tt1375666",
-    Title: "Inception",
-    Year: "2010",
-    Poster:
-      "https://m.media-amazon.com/images/M/MV5BMjAxMzY3NjcxNF5BMl5BanBnXkFtZTcwNTI5OTM0Mw@@._V1_SX300.jpg",
-    runtime: 148,
-    imdbRating: 8.8,
-    userRating: 10,
-  },
-  {
-    imdbID: "tt0088763",
-    Title: "Back to the Future",
-    Year: "1985",
-    Poster:
-      "https://m.media-amazon.com/images/M/MV5BZmU0M2Y1OGUtZjIxNi00ZjBkLTg1MjgtOWIyNThiZWIwYjRiXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_SX300.jpg",
-    runtime: 116,
-    imdbRating: 8.5,
-    userRating: 9,
-  },
-];
-
 const average = (arr) =>
   Number(arr.reduce((acc, cur, i, arr) => acc + cur / arr.length, 0)).toFixed(
     1
@@ -81,15 +34,20 @@ export default function App() {
     setWatched((movies) => movies.filter((m) => m.imdbID !== id));
   };
 
+
+  // This can now be converted to a eventhandler but we keep like this for learning
   useEffect(
     function () {
+      const controller = new AbortController();
+
       async function fetchMovies() {
         try {
           setIsLoading(true);
           setError("");
 
           const res = await fetch(
-            `${OMDB_BASE_URL}?s=${query}&apiKey=${OMDB_API_KEY}`
+            `${OMDB_BASE_URL}?s=${query}&apiKey=${OMDB_API_KEY}`,
+            { signal: controller.signal }
           );
           if (!res.ok) throw new Error("Something went wrong while fetching");
 
@@ -98,8 +56,10 @@ export default function App() {
 
           setMovies(data.Search);
         } catch (err) {
-          console.log(err.message);
-          setError(err.message);
+          if (err.name !== "AbortError") {
+            console.log(err.message);
+            setError(err.message);
+          }
         } finally {
           setIsLoading(false);
         }
@@ -111,7 +71,9 @@ export default function App() {
         return;
       }
 
+      handleCloseMovie();
       fetchMovies();
+      return () => controller.abort();
     },
     [query]
   );
@@ -325,9 +287,18 @@ function MovieDetailView({ selectedId, onCloseMovie, onAddMovie, watched }) {
 
   useEffect(() => {
     if (!title) return;
-    document.title = '🎬' + title;
-    return () => document.title = '🍿 usePopcorn'
+    document.title = "🎬" + title;
+    return () => (document.title = "🍿 usePopcorn");
   }, [title]);
+
+  useEffect(() => {
+    function callback(e) {
+      if (e.code === "Escape") onCloseMovie();
+    }
+
+    document.addEventListener("keydown", callback);
+    return () => document.removeEventListener("keydown", callback);
+  }, [onCloseMovie]);
 
   return (
     <div className="details">
