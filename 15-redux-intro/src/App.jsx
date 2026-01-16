21e558aeab64e322b67965d4252da248974f1bab
Nitin Thakur <ntt45419@gmail.com>
2026-01-17 00:18:48 +0530

    Implemented global store using react redux

--- 15-redux-intro/src/App.jsx
+++ 15-redux-intro/src/App.jsx
@@ -1,20 +1,24 @@
+import { Provider } from "react-redux";
 import {
   AccountOperations,
   BalanceDisplay,
   CreateCustomer,
   Customer,
 } from "./features";
+
 import store from "./store/store";
 import "./index.css";
 function App() {
   return (
-    <div>
-      <h1>🏦 The React-Redux Bank ⚛️</h1>
-      <CreateCustomer />
-      <Customer />
-      <AccountOperations />
-      <BalanceDisplay />
-    </div>
+    <Provider store={store}>
+      <div>
+        <h1>🏦 The React-Redux Bank ⚛️</h1>
+        <CreateCustomer />
+        <Customer />
+        <AccountOperations />
+        <BalanceDisplay />
+      </div>
+    </Provider>
   );
 }
 
