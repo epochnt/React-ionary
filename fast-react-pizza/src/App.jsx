e266f17ebb77e674a4febfa9f5aa9d43f1145043
Nitin Thakur <ntt45419@gmail.com>
2026-01-18 00:36:04 +0530

    Added basic page routing, will update with react rotuer 7 practices later

--- fast-react-pizza/src/App.jsx
+++ fast-react-pizza/src/App.jsx
@@ -1,3 +1,22 @@
+import { createBrowserRouter, RouterProvider, Navigate } from "react-router";
+import { Menu, Cart, Order, CreateOrder } from "./features";
+import Home from "./ui/Home";
+
+// Update element to component and navigate properly using the data syntax
+const rotuer = createBrowserRouter([
+  { path: "/", element: <Home /> },
+  { path: "/menu", element: <Menu /> },
+  { path: "/cart", element: <Cart /> },
+  {
+    path: "/order",
+    children: [
+      { index: true, element: <Navigate to="/new" /> },
+      { path: "new", element: <CreateOrder /> },
+      { path: ":orderId", element: <Order /> },
+    ],
+  },
+]);
+
 export default function App() {
-  return <div>Heelo</div>;
+  return <RouterProvider router={rotuer} />;
 }
