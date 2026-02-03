f2f7deaa7721f869c04b5fe7035427d0ef5b76ba
Nitin Thakur <ntt45419@gmail.com>
2026-02-03 23:31:17 +0530

    Added compound table component

--- the-wild-oasis/src/features/cabins/CabinRow.jsx
+++ the-wild-oasis/src/features/cabins/CabinRow.jsx
@@ -1,4 +1,3 @@
-import { useState } from 'react'
 import { FaTrashAlt, FaPen, FaCopy } from 'react-icons/fa'
 import styled from 'styled-components'
 
@@ -10,18 +9,8 @@ import Spinner from '../../ui/Spinner'
 import CreateCabinForm from './CreateCabinForm'
 import Modal from '../../ui/Modal'
 import ConfirmDelete from '../../ui/ConfirmDelete'
-
-const TableRow = styled.div`
-  display: grid;
-  grid-template-columns: 0.6fr 1.8fr 2.2fr 1fr 1fr 1fr;
-  column-gap: 2.4rem;
-  align-items: center;
-  padding: 1.4rem 2.4rem;
-
-  &:not(:last-child) {
-    border-bottom: 1px solid var(--color-grey-100);
-  }
-`
+import Table from '../../ui/Table'
+import Menus from '../../ui/Menus'
 
 const Img = styled.img`
   display: block;
@@ -79,7 +68,7 @@ export default function CabinRow({
 
   return (
     <>
-      <TableRow role="row">
+      <Table.Row>
         <Img src={image} />
         <Cabin>{name}</Cabin>
         <div>Fits up to {maxCapacity} guests</div>
@@ -130,8 +119,16 @@ export default function CabinRow({
               />
             </Modal.Window>
           </Modal>
+          <Menus.Menu>
+            <Menus.Toggle id={id} />
+            <Menus.List id={id}>
+              <Menus.Button>Duplicate</Menus.Button>
+              <Menus.Button>Edit</Menus.Button>
+              <Menus.Button>Delete</Menus.Button>
+            </Menus.List>
+          </Menus.Menu>
         </Row>
-      </TableRow>
+      </Table.Row>
     </>
   )
 }
